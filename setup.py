6b1fa505adc244d5aace055d395f9358f35fe893
Daniel Gonzalez <gonvaled@gonvaled.com>
2019-09-24 16:39:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,16 +19,21 @@ with open("README.md") as readme:
     long_description = readme.read()
 
 distutils.core.setup(
-    name = "fastjsonrpc",
-    version = "0.3.0",
-    packages = ["fastjsonrpc"],
-    author = "Tadeas Moravec",
-    author_email = "tadeas.moravec@email.cz",
-    url = "http://github.com/tadeas/fastjsonrpc",
-    license = "http://www.apache.org/licenses/LICENSE-2.0",
-    description = "A library for writing asynchronous JSON-RPC servers and clients in Python, using Twisted.",
-    long_description = long_description,
-    classifiers = [
+    name="fastjsonrpc",
+    version="0.4.0",
+    packages=["fastjsonrpc"],
+    author="Tadeas Moravec",
+    author_email="tadeas.moravec@email.cz",
+    url="http://github.com/tadeas/fastjsonrpc",
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    description="A library for writing asynchronous JSON-RPC servers and clients in Python, using Twisted.",
+    long_description=long_description,
+    classifiers=[
         'Development Status :: 4 - Beta',
-        ]
+    ],
+    install_requires=[
+        'pytest==5.1.3',
+        'Twisted==19.7.0',
+        'pyOpenSSL==19.0.0',
+    ]
 )
